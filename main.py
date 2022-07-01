c4bfa03eb68825b40afa81fd03e2f434bc09bbfe
SrsZulumaster <minaolenzimmer@gmail.com>
2022-07-01 19:17:36 +0300

    Getting value from the sales table of last 30 days now returns the value of the sales from yesterday. But if no sales have been made today returns wrong value(needs to be implemented)

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import time
+
 import pyautogui
-import pytesseract as pt
+from datetime import timedelta, date
 from PIL import Image
+from pyperclip import paste
 
 '''
 Buum- a retail program built on Firebird 3.0 - restricted to only use client side application
@@ -10,21 +13,44 @@ Pytesseract is used to locate the correct line on screen will be used to read th
 Future of the project might include web scraping to get info from Sensmax
 '''
 
-# this line is absolutely necessary for Pytesseract to work DO NOT EDIT
-pt.pytesseract.tesseract_cmd = r'C:\Users\szimmerman\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
-# prints out available languages
-print(pt.pytesseract.get_languages(config=''))
-# reads and prints out words on the pictures. lang='est' necessary to get correct output
-print(pt.image_to_string('müük.png', lang='est'))
-print(pt.image_to_string('aruanded.png', lang='est')) # aruanded.png cropped to small
-
-# these lines find buttons on the pictures and click them on screen
-# pyautogui.click('clickON.png')
-# pyautogui.click('müük.png')
-# pyautogui.click('aruanded.png')
+# this clicks on the cell where the column and row intersect and returns the value
+def get_cell_value(column, row):
+    pyautogui.click(column[0] + 10, row[1] + 5, clicks=3)
+    pyautogui.keyDown('ctrl')
+    pyautogui.press('c')
+    pyautogui.keyUp('ctrl')
+    return paste()
 
 
-# these lines will be used to locate the necessary value in the table
-# summaKM = pyautogui.locateOnScreen()
-# KP = pyautogui.locateOnScreen()
+def get_yesterday_sales():
+    # these lines find buttons on the pictures and click them on
+    # these lines open the report needed to get the value
+    pyautogui.click('clickON.png')
+    pyautogui.click('sale_rep.png')
+    pyautogui.click('aruanded.png')
+
+    # sometimes it takes a few seconds to open the report this should compensate for that
+    time.sleep(5)
+
+    # this block uses the line marker arrow and gets to the bottom of the table
+    arrow_start = pyautogui.locateOnScreen('arrow.png')
+    pyautogui.click('arrow.png')
+    pyautogui.press('down', presses=30)
+    arrow_end = pyautogui.locateOnScreen('arrow.png')
+    while arrow_start != arrow_end:
+        arrow_start = pyautogui.locateOnScreen('arrow.png')
+        pyautogui.press('down')
+        arrow_end = pyautogui.locateOnScreen('arrow.png')
+
+    # this selects the second to last line
+    if arrow_start == arrow_end:
+        pyautogui.press('up')
+
+    # these lines find the correct column and line for the sales yesterday
+    sum_km = pyautogui.locateOnScreen('sales.png')
+    arrow_final = pyautogui.locateOnScreen('arrow.png')
+    final_sales = get_cell_value(sum_km, arrow_final)
+    return final_sales
+
+
