789110e5a72313a371b97fbec3f213c2ef3514d6
SrsZulumaster <minaolenzimmer@gmail.com>
2022-07-14 18:11:04 +0300

    Getting value from table now returns the value using pytesseract, without symbols. get_yesterdays_sales() now returns a list containing how many euros worth of items were sold and amount of sales. Both are values needed to enter into the excel file.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import time
-
 import pyautogui
 from datetime import timedelta, date
+import re
+import pytesseract
 from PIL import Image
 from pyperclip import paste
 
@@ -12,21 +13,29 @@ Pyautogui is used to interact with Buum
 Pytesseract is used to locate the correct line on screen will be used to read the date
 Future of the project might include web scraping to get info from Sensmax
 '''
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\szimmerman\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+
+
+# Use this function to strip all unnecessary symbols that pytesseract might pick up
+def get_value_without_symbols(value):
+    value = value.replace('¢', '')
+    value = value.strip()
+    return value
 
 
 # this clicks on the cell where the column and row intersect and returns the value
 def get_cell_value(column, row):
-    pyautogui.click(column[0] + 10, row[1] + 5, clicks=3)
-    pyautogui.keyDown('ctrl')
-    pyautogui.press('c')
-    pyautogui.keyUp('ctrl')
-    return paste()
+    shot = pyautogui.screenshot(region=(column[0], row[1], column[2], row[3]))
+    screen_value = pytesseract.image_to_string(shot)
+    screen_value = get_value_without_symbols(screen_value)
+    return screen_value
 
 
 def get_yesterday_sales():
     # these lines find buttons on the pictures and click them on
     # these lines open the report needed to get the value
     pyautogui.click('clickON.png')
+    time.sleep(0.1)
     pyautogui.click('sale_rep.png')
     pyautogui.click('aruanded.png')
 
@@ -37,20 +46,19 @@ def get_yesterday_sales():
     arrow_start = pyautogui.locateOnScreen('arrow.png')
     pyautogui.click('arrow.png')
     pyautogui.press('down', presses=30)
-    arrow_end = pyautogui.locateOnScreen('arrow.png')
-    while arrow_start != arrow_end:
-        arrow_start = pyautogui.locateOnScreen('arrow.png')
-        pyautogui.press('down')
-        arrow_end = pyautogui.locateOnScreen('arrow.png')
-
+    
     # this selects the second to last line
-    if arrow_start == arrow_end:
-        pyautogui.press('up')
+    pyautogui.press('up')
 
     # these lines find the correct column and line for the sales yesterday
     sum_km = pyautogui.locateOnScreen('sales.png')
+    bills = pyautogui.locateOnScreen('arveid.png')
     arrow_final = pyautogui.locateOnScreen('arrow.png')
     final_sales = get_cell_value(sum_km, arrow_final)
-    return final_sales
+    final_bills = get_cell_value(bills, arrow_final)
+    final_values = [final_sales, final_bills]
+    return final_values
 
 
+sales = get_yesterday_sales()
+print(sales)
