03f4f3b598683b47770bffdf249ab4551d002a0d
SrsZulumaster <minaolenzimmer@gmail.com>
2022-07-26 19:04:45 +0300

    Data compare works rudimentary at best but enough to work on, next will iterate over each row in the table to find the column that matches criteria

--- file_ops.py
+++ file_ops.py
@@ -1,9 +1,21 @@
 from pyexcel_odsr import get_data
 
+'''
+      sheet  line column
+        |      |  |
+data["Andmed"][0][1]
+'''
+
 
 def get_date_yesterday():
-    data = get_data("müük Rocca.ods", start_row=30, row_limit=2, start_column=1, column_limit=11, sheet_name="Andmed")
-    print(data["Andmed"][0][5])
+    data = get_data("müük Rocca.ods", start_row=30, row_limit=100, start_column=1, column_limit=11, sheet_name="Andmed")
+
+    print(data["Andmed"][10][5])
+
+    if data["Andmed"][23][5] == "":
+        print("yeet")
+    else:
+        print("no")
     # return yesterday
 
 
