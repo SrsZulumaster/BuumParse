f77b559a3bc1ded6e48c2f87a4a38ad4ae026c53
SrsZulumaster <minaolenzimmer@gmail.com>
2022-08-17 09:49:22 +0300

    While loop completed and now returns a list with a list of all possible solutions [[* Store1, EAN, Size, amount], [* Store2, EAN, Size, amount]]

--- get_cell.py
+++ get_cell.py
@@ -6,22 +6,31 @@ import pytesseract
 # Originally every value is formatted as 1,0 or just empty but never 0,5(Except for ropes)
 def get_cell_number(column, row):
     custom_config = r'--psm 6  -c tessedit_char_whitelist=0123456789,.'
-    screen_shot = pyautogui.screenshot(region=(column[0],row[1],column[2],row[3]))
+    screen_shot = pyautogui.screenshot(region=(column[0], row[1], column[2], row[3]))
     screen_value = pytesseract.image_to_string(screen_shot, config=custom_config)
     screen_value = screen_value.strip()
     if screen_value == '':
         return None
     else:
-        screen_value = int(screen_value.replace(',', '.'))
+        screen_value = float(screen_value.replace(',', '.'))
         print(screen_value)
         return int(screen_value)
+
 
 # This gets the string values of cells, used mostly to identify the cell with the store name
 # formatted as (* Ülemiste kauplus)
 
 def get_cell_string(column, row):
     custom_config = r'--psm 6'
-    screen_shot = pyautogui.screenshot(region=(column[0],row[1],column[2],row[3]))
+    screen_shot = pyautogui.screenshot(region=(column[0], row[1], column[2], row[3]))
+    screen_value = pytesseract.image_to_string(screen_shot, config=custom_config)
+    screen_value = screen_value.strip()
+    print(screen_value)
+    return screen_value
+
+
+def get_shot_string(screen_shot):
+    custom_config = r'--psm 6'
     screen_value = pytesseract.image_to_string(screen_shot, config=custom_config)
     screen_value = screen_value.strip()
     print(screen_value)
--- max_storage.py
+++ max_storage.py
@@ -1,7 +1,7 @@
 import time
 import pyautogui as pag
 import pytesseract
-from get_cell import get_cell_number, get_cell_string
+from get_cell import get_cell_number, get_cell_string, get_shot_string
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\szimmerman\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
@@ -24,6 +24,7 @@ def send_max_items():
     Johvi = {}
     Kaubamajakas = {}
     pag.click("py_auto_pics/ladu.png")
+    time.sleep(0.3)
     pag.click("py_auto_pics/ladu.png")
     time.sleep(4)
     pag.click("py_auto_pics/Kogus_laos.png")
@@ -36,6 +37,7 @@ def send_max_items():
     size = pag.screenshot(region=(column[0], line[1], column[2], line[3]))
     EAN_cell = pag.locateOnScreen("py_auto_pics/ribakood.png")
     EAN = get_cell_number(row=line, column=EAN_cell)
+    art_size = pag.screenshot
     open_article()
     time.sleep(2)
     send_info = get_send_info(size, EAN)
@@ -66,6 +68,8 @@ def send_max_items():
 # by checking how many items are in each store and how many is allowed
 def get_send_info(size, EAN):
     return_info= []
+    send_to_stores = []
+    size_value = get_shot_string(size)
     time.sleep(1)
     pag.click("py_auto_pics/lao_info.png")
     pag.locateOnScreen(size)
@@ -76,28 +80,37 @@ def get_send_info(size, EAN):
     width = bottom_corner[0] - top_corner[0]
     height = bottom_corner[1] - top_corner[1]
     region = (left_top_coord[0], left_top_coord[1], width, height)
+    size_column = pag.locateOnScreen("py_auto_pics/suurus.png")
     line = pag.locateOnScreen("py_auto_pics/arrow.png", region=region)
     pag.click(line)
-    while True:
-        available_storage = pag.locateOnScreen("py_auto_pics/kogus.png")
-        maximum_allowed = pag.locateOnScreen("py_auto_pics/maksimumkogus.png")
-        available_num = get_cell_number(available_storage, line)
-        max_allowed_num = get_cell_number(maximum_allowed, line)
-        if available_num is not None and max_allowed_num is not None and available_num != 0:
-            if available_num < max_allowed_num:
+
+    while pag.locateOnScreen("py_auto_pics/grillsalong.png") is None:
+        line = pag.locateOnScreen("py_auto_pics/arrow.png", region=region)
+        art_size = pag.screenshot(region=(size_column[0], line[1], size_column[2], line[3]))
+        art_size_value = get_shot_string(art_size)
+        # This checks if the Size is correct, if it is then proceeds to check if there is a reason to send the item
+        if art_size_value == size_value:
+            available_storage = pag.locateOnScreen("py_auto_pics/kogus.png")
+            maximum_allowed = pag.locateOnScreen("py_auto_pics/maksimumkogus.png")
+            available_num = get_cell_number(available_storage, line)
+            max_allowed_num = get_cell_number(maximum_allowed, line)
+
+            # Because some cells can be empty if the max allowed amount hasn't been set
+            # or the store has never had this item in store
+            # This checks if anything is empty and if there is room to send it
+            if available_num is not None and max_allowed_num is not None and available_num < max_allowed_num:
                 check_store = pag.locateOnScreen("py_auto_pics/artikli_ladu.png")
                 send_amount = max_allowed_num - available_num
                 to_store = get_cell_string(check_store, line)
-                return_info = [EAN, send_amount, to_store]
-                break
-
+                return_info = [to_store, EAN, size_value, send_amount]
+                send_to_stores.append(return_info)
+                pag.press('down')
             else:
                 pag.press('down')
         else:
-            line = pag.locateOnScreen("py_auto_pics/arrow.png", region=region)
             pag.press('down')
-        if pag.locateOnScreen("py_auto_pics/grillsalong.png") is not None:
-            break
-    return return_info
+    return send_to_stores
+
+
 
 send_max_items()
